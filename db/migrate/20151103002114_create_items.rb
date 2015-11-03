90f7d655e4d5c2390a7da6d413a043e39db02ea4
Alon Waisman <mowalon@yahoo.com>
2015-11-02 18:17:09 -0700

    Seed data

--- db/migrate/20151103002114_create_items.rb
+++ db/migrate/20151103002114_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration
     create_table :items do |t|
       t.string :name
       t.text :description
-      t.numeric :unit_price
+      t.integer :unit_price
       t.references :merchant, index: true, foreign_key: true
 
       t.timestamps null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,55 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+class Seed
+
+  def initialize
+    create_customers
+    create_merchants
+    create_invoices
+    create_items
+    create_transactions
+    create_invoice_items
+  end
+
+  def data_folder
+    File.join(File.expand_path('../..',  __FILE__), "data")
+  end
+
+  def create_customers
+    CSV.foreach(File.join(data_folder, 'customers.csv'), headers: true, :header_converters => :symbol) do |row|
+      Customer.create(row.to_hash)
+    end
+  end
+
+  def create_merchants
+    CSV.foreach(File.join(data_folder, 'merchants.csv'), headers: true, :header_converters => :symbol) do |row|
+      Merchant.create(row.to_hash)
+    end
+  end
+
+  def create_invoices
+    CSV.foreach(File.join(data_folder, 'invoices.csv'), headers: true, :header_converters => :symbol) do |row|
+      Invoice.create(row.to_hash)
+    end
+  end
+
+  def create_items
+    CSV.foreach(File.join(data_folder, 'items.csv'), headers: true, :header_converters => :symbol) do |row|
+      Item.create(row.to_hash)
+    end
+  end
+
+  def create_transactions
+    CSV.foreach(File.join(data_folder, 'transactions.csv'), headers: true, :header_converters => :symbol) do |row|
+      Transaction.create(row.to_hash)
+    end
+  end
+
+  def create_invoice_items
+    CSV.foreach(File.join(data_folder, 'invoice_items.csv'), headers: true, :header_converters => :symbol) do |row|
+      InvoiceItem.create(row.to_hash)
+    end
+  end
+end
+
+seed = Seed.new
