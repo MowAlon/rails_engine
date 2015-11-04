42c44895c9c3cfed1a391dbb2565bdd13af8a518
Alon Waisman <mowalon@yahoo.com>
2015-11-04 14:53:23 -0700

    Complete top_x_merchants_revenue

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -12,6 +12,30 @@ class Api::V1::MerchantsController < Api::V1::BaseController
     respond_with Invoice.where(merchant_id: params[:id]).order(:id)
   end
 
+  def most_revenue
+    respond_with Merchant.most_revenue(params[:quantity].to_i)
+  end
+
+  def wip
+    # GET /api/v1/merchants/most_revenue?quantity=x returns the top x merchants ranked by total revenue
+
+    # Cool debug output for totaling revenue for a collection of invoices
+    total = invoices.reduce(0){|sum, invoice|puts "$#{invoice.unit_price} * #{invoice.quantity} = $#{invoice.unit_price * invoice.quantity}";sum += (invoice.unit_price * invoice.quantity)};puts "----------------------\n$#{total}"
+
+    # Produces an array of the subtotals for an invoices invoice_items
+    invoice.invoice_items.pluck('unit_price * quantity')
+    # Single query to sum the subtotals of an invoice
+    invoice.invoice_items.sum('unit_price * quantity')
+
+    # Produces a hash of all invoice_ids and corresponding revenue totals
+    InvoiceItem.all.group(:invoice_id).sum("unit_price * quantity")
+
+    # Produces a hash of all successful invoice_ids and corresponding revenue totals
+    successful_invoices = Invoice.joins(:transactions).where(transactions: {result: "success"}).pluck(:id)
+    InvoiceItem.where(invoice_id: successful_invoices).group(:invoice_id).sum('unit_price * quantity')
+
+  end
+
   private
 
     def finder_params
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,2 +1,16 @@
 class Merchant < ActiveRecord::Base
+
+  def self.most_revenue(merchant_count)
+    sorted_revenue_by_merchant.first(merchant_count).map{|merchant, revenue| merchant}
+  end
+
+  private
+
+    def self.sorted_revenue_by_merchant
+      successful_revenue_by_merchant.sort_by{|merchant, revenue| revenue}.reverse
+    end
+
+    def self.successful_revenue_by_merchant
+      Invoice.joins(:invoice_items, :transactions).where(transactions: {result: "success"}).includes(:merchant).group(:merchant).sum('invoice_items.unit_price * invoice_items.quantity')
+    end
 end
